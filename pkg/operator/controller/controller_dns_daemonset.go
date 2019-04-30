8fefc8c81ec725623f5cf364f1263510ea978d18
pocbot <pocbot@localhost>
2019-04-30 14:10:32 +0530

    tracer

--- pkg/operator/controller/controller_dns_daemonset.go
+++ pkg/operator/controller/controller_dns_daemonset.go
@@ -3,21 +3,18 @@ package controller
 import (
 	"context"
 	"fmt"
-
 	operatorv1 "github.com/openshift/api/operator/v1"
 	"github.com/openshift/cluster-dns-operator/pkg/manifests"
-
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-
 	"github.com/sirupsen/logrus"
-
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// ensureDNSDaemonSet ensures the dns daemonset exists for a given dns.
 func (r *reconciler) ensureDNSDaemonSet(dns *operatorv1.DNS, clusterIP, clusterDomain string) (*appsv1.DaemonSet, error) {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
 	desired, err := desiredDNSDaemonSet(dns, clusterIP, clusterDomain, r.CoreDNSImage, r.OpenshiftCLIImage)
 	if err != nil {
 		return nil, fmt.Errorf("failed to build dns daemonset: %v", err)
@@ -38,10 +35,9 @@ func (r *reconciler) ensureDNSDaemonSet(dns *operatorv1.DNS, clusterIP, clusterD
 	}
 	return r.currentDNSDaemonSet(dns)
 }
-
-// ensureDNSDaemonSetDeleted ensures deletion of daemonset and related resources
-// associated with the dns.
 func (r *reconciler) ensureDNSDaemonSetDeleted(dns *operatorv1.DNS) error {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
 	daemonset := &appsv1.DaemonSet{}
 	name := DNSDaemonSetName(dns)
 	daemonset.Name = name.Name
@@ -55,27 +51,19 @@ func (r *reconciler) ensureDNSDaemonSetDeleted(dns *operatorv1.DNS) error {
 	}
 	return nil
 }
-
-// desiredDNSDaemonSet returns the desired dns daemonset.
 func desiredDNSDaemonSet(dns *operatorv1.DNS, clusterIP, clusterDomain, coreDNSImage, openshiftCLIImage string) (*appsv1.DaemonSet, error) {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
 	daemonset := manifests.DNSDaemonSet()
 	name := DNSDaemonSetName(dns)
 	daemonset.Name = name.Name
 	daemonset.Namespace = name.Namespace
 	daemonset.SetOwnerReferences([]metav1.OwnerReference{dnsOwnerRef(dns)})
-
-	daemonset.Labels = map[string]string{
-		// associate the daemonset with the dns
-		manifests.OwningDNSLabel: DNSDaemonSetLabel(dns),
-	}
-
-	// Ensure the daemonset adopts only its own pods.
+	daemonset.Labels = map[string]string{manifests.OwningDNSLabel: DNSDaemonSetLabel(dns)}
 	daemonset.Spec.Selector = DNSDaemonSetPodSelector(dns)
 	daemonset.Spec.Template.Labels = daemonset.Spec.Selector.MatchLabels
-
 	coreFileVolumeFound := false
 	for i := range daemonset.Spec.Template.Spec.Volumes {
-		// TODO: remove hardcoding of volume name
 		if daemonset.Spec.Template.Spec.Volumes[i].Name == "config-volume" {
 			daemonset.Spec.Template.Spec.Volumes[i].ConfigMap.Name = DNSConfigMapName(dns).Name
 			coreFileVolumeFound = true
@@ -85,7 +73,6 @@ func desiredDNSDaemonSet(dns *operatorv1.DNS, clusterIP, clusterDomain, coreDNSI
 	if !coreFileVolumeFound {
 		return nil, fmt.Errorf("volume 'config-volume' is not found")
 	}
-
 	for i, c := range daemonset.Spec.Template.Spec.Containers {
 		switch c.Name {
 		case "dns":
@@ -94,18 +81,11 @@ func desiredDNSDaemonSet(dns *operatorv1.DNS, clusterIP, clusterDomain, coreDNSI
 			daemonset.Spec.Template.Spec.Containers[i].Image = openshiftCLIImage
 			envs := []corev1.EnvVar{}
 			if len(clusterIP) > 0 {
-				envs = append(envs, corev1.EnvVar{
-					Name:  "NAMESERVER",
-					Value: clusterIP,
-				})
+				envs = append(envs, corev1.EnvVar{Name: "NAMESERVER", Value: clusterIP})
 			}
 			if len(clusterDomain) > 0 {
-				envs = append(envs, corev1.EnvVar{
-					Name:  "CLUSTER_DOMAIN",
-					Value: clusterDomain,
-				})
+				envs = append(envs, corev1.EnvVar{Name: "CLUSTER_DOMAIN", Value: clusterDomain})
 			}
-
 			if daemonset.Spec.Template.Spec.Containers[i].Env == nil {
 				daemonset.Spec.Template.Spec.Containers[i].Env = []corev1.EnvVar{}
 			}
@@ -114,9 +94,9 @@ func desiredDNSDaemonSet(dns *operatorv1.DNS, clusterIP, clusterDomain, coreDNSI
 	}
 	return daemonset, nil
 }
-
-// currentDNSDaemonSet returns the current dns daemonset.
 func (r *reconciler) currentDNSDaemonSet(dns *operatorv1.DNS) (*appsv1.DaemonSet, error) {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
 	daemonset := &appsv1.DaemonSet{}
 	if err := r.client.Get(context.TODO(), DNSDaemonSetName(dns), daemonset); err != nil {
 		if errors.IsNotFound(err) {
@@ -126,39 +106,36 @@ func (r *reconciler) currentDNSDaemonSet(dns *operatorv1.DNS) (*appsv1.DaemonSet
 	}
 	return daemonset, nil
 }
-
-// createDNSDaemonSet creates a dns daemonset.
 func (r *reconciler) createDNSDaemonSet(daemonset *appsv1.DaemonSet) error {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
 	if err := r.client.Create(context.TODO(), daemonset); err != nil {
 		return fmt.Errorf("failed to create dns daemonset %s/%s: %v", daemonset.Namespace, daemonset.Name, err)
 	}
 	logrus.Infof("created dns daemonset: %s/%s", daemonset.Namespace, daemonset.Name)
 	return nil
 }
-
-// updateDNSDaemonSet updates a dns daemonset.
 func (r *reconciler) updateDNSDaemonSet(current, desired *appsv1.DaemonSet) error {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
 	changed, updated := daemonsetConfigChanged(current, desired)
 	if !changed {
 		return nil
 	}
-
 	if err := r.client.Update(context.TODO(), updated); err != nil {
 		return fmt.Errorf("failed to update dns daemonset %s/%s: %v", updated.Namespace, updated.Name, err)
 	}
 	logrus.Infof("updated dns daemonset: %s/%s", updated.Namespace, updated.Name)
 	return nil
 }
-
-// daemonsetConfigChanged checks if current config matches the expected config
-// for the dns daemonset and if not returns the updated config.
 func daemonsetConfigChanged(current, expected *appsv1.DaemonSet) (bool, *appsv1.DaemonSet) {
+	_logClusterCodePath()
+	defer _logClusterCodePath()
 	changed := false
 	updated := current.DeepCopy()
 	for _, name := range []string{"dns", "dns-node-resolver"} {
 		var curIndex int
 		var curImage, expImage string
-
 		for i, c := range current.Spec.Template.Spec.Containers {
 			if name == c.Name {
 				curIndex = i
@@ -172,7 +149,6 @@ func daemonsetConfigChanged(current, expected *appsv1.DaemonSet) (bool, *appsv1.
 				break
 			}
 		}
-
 		if len(curImage) == 0 {
 			logrus.Errorf("current daemonset %s/%s did not contain expected %s container", current.Namespace, current.Name, name)
 			updated.Spec.Template.Spec.Containers = expected.Spec.Template.Spec.Containers
@@ -183,8 +159,6 @@ func daemonsetConfigChanged(current, expected *appsv1.DaemonSet) (bool, *appsv1.
 			changed = true
 		}
 	}
-	// TODO: Also check Env and Volume sources?
-
 	if !changed {
 		return false, nil
 	}
